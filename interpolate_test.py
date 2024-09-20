877f6d0a5fc3e8b661cd894c1142fe32f2718e8f
Daniel <131325098+dddanielliu@users.noreply.github.com>
2024-09-21 00:21:05 +0800

    example update

--- interpolate_test.py
+++ interpolate_test.py
@@ -2,7 +2,12 @@ from InterpolatorInterface import InterpolatorInterface
 interpolator = InterpolatorInterface()
 result = interpolator.generate(
     imgs=('img0.png', 'img1.png'),
-    exp=1
-    # outputdir="interpolator_out"
+    exp=3,
+    outputdir="interpolator_out"
+)
+print(result)
+result = interpolator.generate(
+    imgs=('img0.png', 'img1.png'),
+    exp=3
 )
 print(result)
